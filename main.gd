d50367868b7ff1b43525a331df4b0dca5883e4fe
Johannes Danneker <fry-@users.noreply.github.com>
2016-06-09 21:34:18 +0200

    make obstacles move

--- main.gd
+++ main.gd
@@ -6,17 +6,21 @@ var init_pos_y
 var tile_size = 64
 var segment = preload("res://segment.tscn")
 var time = 0
+var total_time = 0
 
 var obstacle
 
 func _ready():
 	init_pos_x = get_viewport_rect().size.x + (2 * tile_size)
-	init_pos_y = (get_viewport_rect().size.y / 2) - (1.5 * tile_size)
+	init_pos_y = (get_viewport_rect().size.y / 2) - tile_size
 	var new_segment = segment.instance()
 	add_child(new_segment)
-	get_child(0).set_pos(Vector2(get_viewport_rect().size.x-1.5*tile_size,get_viewport_rect().size.y/2))
+	get_child(get_child_count()-1).set_pos(Vector2(init_pos_x-162,get_viewport_rect().size.y/2))
+	
+	set_fixed_process(true)
 	pass
 
 func _fixed_process(delta):
+	total_time += delta
 	time += delta
 	pass
--- segment.gd
+++ segment.gd
@@ -3,24 +3,37 @@ extends Node2D
 
 var init_pos_y
 var tile_size
+var total_time
 var obstacle = preload("res://obstacle.tscn")
 var colors = [Color(1,0,0), Color(0,0,1)]
 
 func _ready():
 	init_pos_y = get_parent().init_pos_y
 	tile_size = get_parent().tile_size
+	total_time = get_parent().total_time
 	
 	randomize()
 	var pick_color = randi()%2
 	make_obstacle(init_pos_y,pick_color,1)
 	make_obstacle(-init_pos_y,(pick_color+1)%2,2)
+	
+	set_fixed_process(true)
+	pass
+
+func _fixed_process(delta):
+	if get_child_count() == 0:
+		queue_free()
+	for child in get_children():
+		child.move(Vector2(-5*(1 + total_time/60),0))
+		if child.get_global_pos().x < (-2 * tile_size):
+			child.queue_free()
+	
 	pass
 
 func make_obstacle(pos_y,color_id,layer):
 	add_child(obstacle.instance())
 	var num = get_child_count()-1
-	print(num)
 	get_child(num).set_pos(Vector2(0,pos_y))
 	get_child(num).get_child(1).set_modulate(colors[color_id])
-	get_child(num).get_child(0).set_layer_mask(layer)
+	get_child(num).set_layer_mask(layer)
 	pass
