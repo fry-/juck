383ef70a2c4c5b15d6b8c76b242cb02bbe3bb475
Johannes Danneker <fry-@users.noreply.github.com>
2016-06-19 16:25:39 +0200

    add sound and invincible ghost mode after hitting an obstacle

--- obstacle.gd
+++ obstacle.gd
@@ -3,8 +3,10 @@ extends KinematicBody2D
 
 var tile_size
 var collision_counter = 0
+var player
 
 func _ready():
+	player = get_parent().get_parent().get_node("player")
 	tile_size = get_parent().tile_size
 	set_fixed_process(true)
 	pass
@@ -15,6 +17,5 @@ func _fixed_process(delta):
 	move(Vector2(-1,0) * global.MOTION_SPEED * delta)
 	
 	if is_colliding():
-		collision_counter += 1
-		print("obstacle collides: ",collision_counter)
+		player.colliding = true
 	pass
--- player.gd
+++ player.gd
@@ -8,6 +8,8 @@ var current_anim
 var frequency
 var current_color
 var color_changing = false
+var colliding = false
+var current_time = -2
 
 func _ready():
 	randomize()
@@ -18,6 +20,8 @@ func _ready():
 
 func _fixed_process(delta):
 	jump_button = Input.is_action_pressed("jump")
+	#if jump_button == true:
+		#print("jump pressed")
 	duck_button = Input.is_action_pressed("duck")
 	anim_node = get_node("animation")
 	current_anim = anim_node.get_current_animation()
@@ -25,6 +29,7 @@ func _fixed_process(delta):
 	
 	#if current_anim == "run":
 	if jump_button and current_anim == "run":
+		#print("jumping")
 		anim_node.set_current_animation("jump")
 		anim_node.set_speed(frequency*1.1)
 	elif duck_button and current_anim == "run":
@@ -37,6 +42,22 @@ func _fixed_process(delta):
 		color_changing = false
 		current_color = (current_color+1)%2
 		change_color(self)
+	
+	if colliding == true:
+		print("1")
+		colliding = false
+		current_time = get_parent().total_time
+		set_opacity(0.5)
+		set_one_way_collision_direction(Vector2(-1,0))
+		set_collision_mask(3)
+	elif (get_one_way_collision_direction() == Vector2(-1,0)) and ((current_time + 0.4) < get_parent().total_time):
+		print("2")
+		set_one_way_collision_direction(Vector2(0,0))
+	elif (current_time > 0) and ((current_time + 1) < get_parent().total_time):
+		print("3")
+		current_time = -2
+		set_opacity(1)
+		set_collision_mask(global.layer[current_color]) # set the mask for collision
 
 func change_color(node):
 	for child in node.get_children():
@@ -45,3 +66,4 @@ func change_color(node):
 			change_color(child)
 			child.set_modulate(global.colors[current_color])
 	set_collision_mask(global.layer[current_color]) # set the mask for collision
+	print("player: ",get_collision_mask())
